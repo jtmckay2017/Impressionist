9350e47e7534416a296f7a06dbe681e9ac87c189
Joel McKay <jtmckay2017@gmail.com>
2019-01-29 20:00:42 -0500

    initial commit

---
+++ LineBrush.cpp
@@ -0,0 +1,60 @@
+//
+// LineBrush.cpp
+//
+// The implementation of Line Brush. It is a kind of ImpBrush. All your brush implementations
+// will look like the file with the different GL primitive calls.
+//
+
+#include "impressionistDoc.h"
+#include "impressionistUI.h"
+#include "linebrush.h"
+
+extern float frand();
+
+LineBrush::LineBrush(ImpressionistDoc* pDoc, char* name) :
+	ImpBrush(pDoc, name)
+{
+}
+
+void LineBrush::BrushBegin(const Point source, const Point target)
+{
+	ImpressionistDoc* pDoc = GetDocument();
+	ImpressionistUI* dlg = pDoc->m_pUI;
+
+	int size = pDoc->getSize();
+
+
+
+	glLineWidth((float)size); //add slider for line width
+
+	BrushMove(source, target);
+}
+
+void LineBrush::BrushMove(const Point source, const Point target)
+{
+	ImpressionistDoc* pDoc = GetDocument();
+	ImpressionistUI* dlg = pDoc->m_pUI;
+
+	if (pDoc == NULL) {
+		printf("LineBrush::BrushMove  document is NULL\n");
+		return;
+	}
+
+	int size = pDoc->getSize();
+
+	//--------------------
+	//Begin drawing lines
+	//--------------------
+	glBegin(GL_LINES);
+		SetColor(source);
+
+		glVertex2d(target.x, target.y);
+		glVertex2d(target.x+size, target.y+size); //Affected by size slider
+	glEnd();
+}
+
+void LineBrush::BrushEnd(const Point source, const Point target)
+{
+	printf("stopped line brush\n");
+}
+
